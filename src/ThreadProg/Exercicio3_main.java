4392e415a071e3ab4e40617a664baeb4b8bf9a5e
Mario Junior <mariojralves2006@gmail.com>
2023-09-04 14:22:44 -0300

    Exercício 3

--- src/ThreadProg/Exercicio3_main.java
+++ src/ThreadProg/Exercicio3_main.java
@@ -1,45 +1,47 @@
 package ThreadProg;
-import java.util.LinkedList;
-import java.util.List;
 
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Exercicio3_main {
+
     public static void main(String[] args) {
-        int n = 10; // Tamanho do vetor
+        int n = 20; // Tamanho do vetor
         int[] vetor = new int[n];
+
+        // Preencha o vetor com valores de exemplo
         for (int i = 0; i < n; i++) {
             vetor[i] = i;
         }
 
-        List<Integer> listaEncadeada = new LinkedList<>();
+        LinkedList<Integer> lista = new LinkedList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(1);
 
-        int numThreads = n; // Uma thread para cada elemento do vetor
-        Thread[] threads = new Thread[numThreads];
-
-        int elementsPerThread = n / numThreads;
-        int remainingElements = n % numThreads;
-
-        int startIndex = 0;
-        for (int i = 0; i < numThreads; i++) {
-            int endIndex = startIndex + elementsPerThread - 1;
-            if (i == numThreads - 1) {
-                endIndex += remainingElements;
-            }
-
-            threads[i] = new MoverDados(vetor, listaEncadeada, startIndex, endIndex);
+        // Crie e inicie threads para adicionar elementos à lista
+        Thread[] threads = new Thread[n];
+        for (int i = 0; i < n; i++) {
+            final int index = i;
+            threads[i] = new Thread(() -> {
+                synchronized (lista) {
+                    lista.add(vetor[index]);
+                }
+            });
             threads[i].start();
-
-            startIndex = endIndex + 1;
         }
 
-        try {
-            for (Thread thread : threads) {
-                thread.join();
+        // Aguarde até que todas as threads tenham concluído
+        for (int i = 0; i < n; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
 
-        System.out.println("Lista encadeada resultante: " + listaEncadeada);
+        // Imprima a lista encadeada na mesma ordem do vetor
+        for (Integer item : lista) {
+            System.out.print(item + " ");
+        }
     }
 }
